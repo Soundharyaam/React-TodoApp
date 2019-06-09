2604676af73112b5f011639bf04c9e7249762eef
Soundharyaam <soundharyabharatiya@gmail.com>
2019-06-09 19:44:10 +0530

    Updates

---
+++ precache-manifest.9e2137ea55e5c8c674a69ae743c25dd3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ccc8f01bfbd79eb985d4",
+    "url": "/React-TodoApp/static/js/runtime~main.ccc8f01b.js"
+  },
+  {
+    "revision": "6f9d71f114c7b044b585",
+    "url": "/React-TodoApp/static/js/main.6f9d71f1.chunk.js"
+  },
+  {
+    "revision": "a99f5d5e4a61ac3b0916",
+    "url": "/React-TodoApp/static/js/1.a99f5d5e.chunk.js"
+  },
+  {
+    "revision": "6f9d71f114c7b044b585",
+    "url": "/React-TodoApp/static/css/main.c2995ef7.chunk.css"
+  },
+  {
+    "revision": "b06504af4e4dbf20e4b8c5cf6ef841a7",
+    "url": "/React-TodoApp/index.html"
+  }
+];
